279a6d082b9030d456d568017e277ffad1687845
Mau Jiménez <kazz.deer@gmail.com>
2025-02-13 19:45:01 -0600

    printing the amount of words in the book

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+    
+    return file_contents
+
+def word_count(book_as_string):
+    return len(book_as_string.split())
 
 
-main()
+if __name__ == "__main__":
+    book = main()
+    words = word_count(book)
+    print(words)
