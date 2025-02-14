8a76a883eb094f925ec6be8b6e1d74a16c631bcf
Mau Jiménez <kazz.deer@gmail.com>
2025-02-13 19:53:41 -0600

    printing a dictionary of letters and their appearence in the book

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ def main():
 def word_count(book_as_string):
     return len(book_as_string.split())
 
+def letter_count(book_as_string):
+    letter_dict = dict()
+    for letter in book_as_string:
+        if letter.lower() in letter_dict:
+            letter_dict[letter.lower()] = letter_dict[letter.lower()] + 1
+        else:
+            letter_dict[letter.lower()] = 1
+    
+    return letter_dict
+
 
 if __name__ == "__main__":
     book = main()
     words = word_count(book)
-    print(words)
+    letters = letter_count(book)
+    print(letters)
